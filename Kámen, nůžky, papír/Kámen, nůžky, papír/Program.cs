1db63f692b609e9be20248053bc3956a220642fc
Stefanito_2104 <sstanivukovic1@gmail.com>
2023-10-03 10:11:50 +0200

    kamen, nuzky, papir

--- Kámen, nůžky, papír/Kámen, nůžky, papír/Program.cs
+++ Kámen, nůžky, papír/Kámen, nůžky, papír/Program.cs
@@ -12,42 +12,46 @@ namespace Kámen__nůžky__papír
         static void Main(string[] args)
         {
             Console.WriteLine("Kámen = 1, nůžky = 2, papír = 3");
+            int material = Convert.ToInt32(Console.ReadLine());
             Random rnd = new Random();
-            int pc;
-            int material;
-            material = Convert.ToInt32(Console.ReadLine());
-            pc = rnd.Next(1, 3);
-            while ((material > 1) || (material < 3))    
-                if (material == pc)
+            int kompl = rnd.Next(1, 4);
+            while ((material >= 1) || (material <= 3))
+            {
+                if (material == kompl)
                 {
                     Console.WriteLine("Nikdo nevyhrává");
                 }
-                else if ((material == 1) && (pc == 2))
+                else if ((material == 1) && (kompl == 2))
                 {
                     Console.WriteLine("Vyhráváš, kámen porazí nůžky");
                 }
-                else if ((material == 1) && (pc == 3))
+                else if ((material == 1) && (kompl == 3))
                 {
                     Console.WriteLine("Prohráváš, papír porazí kámen");
                 }
-                else if ((material == 2) && (pc == 1))
+                else if ((material == 2) && (kompl == 1))
                 {
                     Console.WriteLine("Prohráváš, kámen porazí nůžky");
                 }
-                else if ((material == 2) && (pc == 3)) 
+                else if ((material == 2) && (kompl == 3))
                 {
                     Console.WriteLine("Vyhráváš, nůžky porazí papír");
                 }
-                else if ((material == 3) && (pc == 1))
+                else if ((material == 3) && (kompl == 1))
                 {
                     Console.WriteLine("Vyhráváš, papír porazí kámen");
                 }
-                else if ((material == 3) && (pc == 2))
+                else if ((material == 3) && (kompl == 2))
                 {
                     Console.WriteLine("Prohráváš, nůžky porazí papír");
                 }
-                pc = rnd.Next(1, 3);
+                else
+                {
+                    Console.WriteLine("Zadej správnou hodnotu");
+                }
                 material = Convert.ToInt32(Console.ReadLine());
+                kompl = rnd.Next(1, 4);
+            }
         }
     }
 }
