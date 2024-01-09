7bcc195625af9d11cb31c88154845697896bc659
Stefanito_2104 <sstanivukovic1@gmail.com>
2024-01-09 11:23:02 +0100

    Survive The Gulag

---
+++ Survive The Gulag/Survive The Gulag/Enemy.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Survive_The_Gulag
+{
+    internal class Enemy
+    {
+        public int enemyHealth, yourHealth;
+        public Enemy(int enemyHealth, int yourHealth) 
+        {
+            this.enemyHealth = enemyHealth;
+            this.yourHealth = yourHealth;
+        }
+        public void Fight()
+        {
+            Console.WriteLine("Ayo, a fight!");
+            while (enemyHealth > 0 || yourHealth > 0)
+            {
+                Random rnd = new Random();
+                int avoidChance = rnd.Next(1, 4);
+                if (avoidChance == 1) 
+                {
+                    Console.WriteLine("Miss!");
+                }
+                else
+                {
+                    int criticalChance = rnd.Next(1, 6);
+                    if (criticalChance == 1) 
+                    {
+                        Console.WriteLine("Critical Hit!");
+                        enemyHealth = enemyHealth-10;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Hit!");
+                        enemyHealth = enemyHealth-5;
+                    }
+                }
+                if (enemyHealth > 0)
+                {
+
+                }
+            }
+            if (yourHealth > 0)
+            {
+                Console.WriteLine("You Win");
+            }
+            else
+            {
+                Console.WriteLine("Game Over!");
+            }
+        }
+    }
+}
--- Survive The Gulag/Survive The Gulag/Program.cs
+++ Survive The Gulag/Survive The Gulag/Program.cs
@@ -10,8 +10,16 @@ namespace Survive_The_Gulag
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Date:                    1946-04-22\nLocation:                Vorkuta Corrective Labor Camp\nSubject Name:            Yevgeniy Aleksandrov\nDate of Birth:           1917-08-16\nPlace of Origin:         Saratov, USSR\nID Number:               F319\nReason of Imprisonment:  Betrayal of The State\nSentence:                10 Years of Hard Labor\n\n\n\n");
-            Console.WriteLine("Doctor: Okay, that should be it. See you in a week, F319.");
+            int health = 100;
+            Console.WriteLine("Date:                    1946-04-22\nLocation:                Vorkuta Corrective Labor Camp\nSubject Name:            Yevgeniy Aleksandrov\nDate of Birth:           1921-08-16\nPlace of Origin:         Saratov, USSR\nID Number:               F319\nReason of Imprisonment:  Betrayal of The State\nSentence:                10 Years of Hard Labor\n\n\n\n");
+            Console.WriteLine("*Breakfast*\n");
+            Random rnd = new Random();
+            int fightChance = rnd.Next(1, 6);
+            if (fightChance == 1) 
+            {
+                Enemy enemy1 = new Enemy(10, health);
+                enemy1.Fight();
+            }
             Console.ReadKey();
         }
     }
