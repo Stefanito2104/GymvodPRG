cff540ee89c4e88fa1dc4af9d2fb82300914b562
Stefanito_2104 <sstanivukovic1@gmail.com>
2024-01-09 07:27:00 +0100

    Survive The Gulag

---
+++ Survive The Gulag/Survive The Gulag/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Survive_The_Gulag
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Date:                    1946-04-22\nLocation:                Vorkuta Corrective Labor Camp\nSubject Name:            Yevgeniy Aleksandrov\nDate of Birth:           1917-08-16\nPlace of Origin:         Saratov, USSR\nID Number:               F319\nReason of Imprisonment:  Betrayal of The State\nSentence:                10 Years of Hard Labor\n\n\n\n");
+            Console.WriteLine("Doctor: Okay, that should be it. See you in a week, F319.");
+            Console.ReadKey();
+        }
+    }
+}
